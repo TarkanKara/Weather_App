b806564d38b93e9875702f20bfa8a124249bf52e
TarkanKara <unnahmet23@hotmail.com>
2022-11-09 11:17:17 +0300

    project setup

---
+++ lib/core/app_asset.dart
@@ -0,0 +1,14 @@
+class appAssets {
+  static const String rain1 = "assets/images/image1.svg";
+  static const String rain2 = "assets/images/image2.svg";
+  static const String cloudy = "assets/images/image3.svg";
+  static const String thunderstorm = "assets/images/image4.svg";
+  static const String closed = "assets/images/image5.svg";
+  static const String warning = "assets/images/1iamge.svg";
+  static const String thunderstorm2 = "assets/images/2iamge.svg";
+  static const String rain3 = "assets/images/3iamge.svg";
+  static const String windy = "assets/images/Group1.svg";
+  static const String windy2 = "assets/images/Group2.svg";
+  static const String windy3 = "assets/images/Group3.svg";
+  static const String windy4 = "assets/images/Group4.svg";
+}
---
+++ lib/models/weather_location.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:weather_app_ui/core/app_asset.dart';
+
+class WeatherLocation {
+  final String city;
+  final String temparature;
+  final String dateTime;
+  final String weatherType;
+  final String iconUrl;
+  String? kelembaban;
+  String? airPressure;
+  String? windSpeed;
+  String? fog;
+
+  WeatherLocation({
+    required this.city,
+    required this.temparature,
+    required this.dateTime,
+    required this.weatherType,
+    required this.iconUrl,
+    this.kelembaban,
+    this.airPressure,
+    this.windSpeed,
+    this.fog,
+  });
+}
+
+final locationList = [
+  WeatherLocation(
+    city: 'Hujan Berawan',
+    temparature: '18\u2103',
+    dateTime: "Senin, 20 Desember 2021",
+    weatherType: 'morning',
+    iconUrl: appAssets.rain1,
+    kelembaban: "86 %",
+    airPressure: "940 hpa",
+    windSpeed: "1 km/h",
+    fog: "14 %",
+  ),
+  WeatherLocation(
+    city: 'Hujan Selasa',
+    temparature: '18\u2103',
+    dateTime: "Senin, 20 Desember 2021",
+    weatherType: 'morning',
+    iconUrl: appAssets.rain1,
+    kelembaban: "86 %",
+    airPressure: "940 hpa",
+    windSpeed: "1 km/h",
+    fog: "14 %",
+  ),
+];
