5bb50b9f550fb3395f23cf39b8a9c1abbcfee608
TarkanKara <unnahmet23@hotmail.com>
2022-11-09 11:40:29 +0300

    DropdownButton

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  String dropdownvalue = 'Tanjungsiang, Subang';
+  var items = [
+    'Tanjungsiang, Subang',
+    'İSTANBUL',
+    'ANKARA',
+    'İZMİR',
+  ];
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -10,6 +17,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffE5E5E5),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: Row(
+            children: [
+              DropdownButton(
+                value: widget.dropdownvalue,
+                icon: const Icon(Icons.keyboard_arrow_down),
+                items: widget.items.map((itemss) {
+                  return DropdownMenuItem(
+                    value: itemss,
+                    child: Text(
+                      itemss,
+                    ),
+                  );
+                }).toList(),
+                onChanged: (String? newValuee) {
+                  setState(() {
+                    widget.dropdownvalue = newValuee!;
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
